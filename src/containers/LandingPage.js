70f6ad03bce6e0c5b19d2a95edf658e1833a431f
ritchey0713 <ritchey0713@gmail.com>
2018-11-07 10:30:30 -0500

    add landing page

---
+++ src/containers/LandingPage.js
@@ -0,0 +1,7 @@
+import React,{ Component } from 'react'
+
+const LandingPage = () => {
+  <div>
+    <h1>Marvel Heros, landing page</h1>
+  </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from './store.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 serviceWorker.unregister();
